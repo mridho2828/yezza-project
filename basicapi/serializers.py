4041bc88000be8c3216fcf252f6bb27e735531b1
Muhammad Ridho Ananda <mridho2828@gmail.com>
2022-04-22 03:08:20 +0700

    modify serializers

--- basicapi/serializers.py
+++ basicapi/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
+from rest_framework.fields import ListField
 from .models import Note
+
+
+class StringArrayField(ListField):
+    def to_representation(self, obj):
+        obj = super().to_representation(obj)
+        return ",".join([str(element) for element in obj])
+
+    def to_internal_value(self, data):
+        return super().to_internal_value(data)
 
 
 class NoteSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
+
+    attachments = StringArrayField()
