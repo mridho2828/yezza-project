6b24226c039494d68a4c39c68de49af202193e1a
Muhammad Ridho Ananda <mridho2828@gmail.com>
2022-04-22 01:57:47 +0700

    migrate to postgres and modify model

--- basicapi/models.py
+++ basicapi/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Note(models.Model):
-    note = models.TextField()
+    author = models.CharField(max_length=255)
+    text = models.TextField()
+    attachments = ArrayField(
+        ArrayField(models.CharField(max_length=255)),
+        null=True,
+        default=None
+    )
 
     def __str__(self):
-        return self.note
+        return self.id
